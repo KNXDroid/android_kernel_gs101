ed08dacb2ed4a3b9c91c53d9f45b4be897b10920
danya2271 <dany2271@yandex.ru>
2025-11-11 11:13:19 +0500

    Revert "sched/fair: Refactor can_migrate_task() to eliminate looping"
    This reverts commit a3a538b6041c289bd28af76f4c7dcc7bab7fa6bc.

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -8626,11 +8626,12 @@ int can_migrate_task(struct task_struct *p, struct lb_env *env)
 			return 0;
 
 		/* Prevent to re-select dst_cpu via env's CPUs: */
-		cpu = cpumask_first_and_and(env->dst_grpmask, env->cpus, p->cpus_ptr);
-
-		if (cpu < nr_cpu_ids) {
-			env->flags |= LBF_DST_PINNED;
-			env->new_dst_cpu = cpu;
+		for_each_cpu_and(cpu, env->dst_grpmask, env->cpus) {
+			if (cpumask_test_cpu(cpu, p->cpus_ptr)) {
+				env->flags |= LBF_DST_PINNED;
+				env->new_dst_cpu = cpu;
+				break;
+			}
 		}
 
 		return 0;
